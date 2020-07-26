e3bc63a3ffe6ac99983df983a965eaf85fa58929
Jarek Lipski <loomchild@loomchild.net>
2020-07-26 20:05:09 +0200

    named tabs

--- pages/index.vue
+++ pages/index.vue
@@ -9,19 +9,19 @@
       <v-col cols="12" md="6" offset-md="3">
         <v-card>
           <v-tabs v-model="tab" background-color="grey lighten-4">
-            <v-tab>
+            <v-tab href="#one">
               One
             </v-tab>
-            <v-tab>
+            <v-tab href="#two">
               Two
             </v-tab>
-            <v-tab-item>
+            <v-tab-item value="one">
               <v-card-text>
                 This is ONE!<br>
                 1111111111
               </v-card-text>
             </v-tab-item>
-            <v-tab-item>
+            <v-tab-item value="two">
               <v-card-text>
                 This is TWO!<br>
                 2222222222
